3ea080fcfd6211a8308250a160ddc4aebd356948
Daniel Tafolla <149893802+DanTaft@users.noreply.github.com>
2025-03-16 21:35:03 -0600

    event
    Notes to event

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
 //scripts for motivation.html
+
+document.getElementById('reveal-quote-btn').addEventListener('click', () => {
+    const quote = document.getElementById('quote');
+    quote.classList.toggle('hidden');
+});
