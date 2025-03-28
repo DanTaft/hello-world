c341d84ac6129c57955c516f087d1dbc0b8c7a0d
Daniel Tafolla <149893802+DanTaft@users.noreply.github.com>
2025-03-28 15:47:29 -0600

    file changes
    renaming of files , changed content for clarity

--- event.js
+++
@@ -1,6 +0,0 @@
-//scripts for motivation.html
-
-document.getElementById('reveal-quote-btn').addEventListener('click', () => {
-    const quote = document.getElementById('quote');
-    quote.classList.toggle('hidden');
-});
